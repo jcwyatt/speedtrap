ac02b275a760dab4a5e19104a443c68da069dd29
Jason Wyatt <jwyatt043@googlemail.com>
2017-10-05 20:13:12 +0100

    Add files via upload
    Includes validation of times by format and values.

--- speedtrap.py
+++ speedtrap.py
@@ -1,19 +1,51 @@
 import datetime
 #speed trap
+
+def timeVerify(t):
+
+	#check the length
+	if len(t) != 8:
+		print(t, "invalid length, must be hh:mm:ss")
+		return False
+
+	#check for numbers in the right places
+
+	numPosns=(0,1,3,4,6,7)
+	for i in numPosns:
+		if not t[i].isnumeric():
+			print (t,"invalid time format, must be numeric eg 01:04:02.")
+			return False
+
+	#check values are valid times
+	if int(t[0:2])>23 or int(t[3:5])>59 or int(t[6:8])>59:
+		print(t,"invalid time. Check values for hours, minutes, seconds.")
+		return False
+
+	#check format has correct separators:
+	colonPosns=(2,5)
+	for i in colonPosns:
+		if t[i]!=":":
+			print (t, "invalid separator. Must be ':'")
+			return False
+
+	return True
+
+
 print("Speed Trap Calculator. Distance = 1 Mile.")
 
 dist = 1
+#input("All times are 6 digit 24hr format, eg:03:44:02\nPress 'Enter' key to continue.")
+#t1 = input("Enter time through first gate : (hh:mm:ss) : ")
+#t2 = input("Enter time through second gate : (hh:mm:ss) : ")
 
-#t1 = input("Time through first gate : (hh:mm:ss) : ")
-#t2 = input("Time through second gate : (hh:mm:ss) : ")
+#print(t2,t1)
 
-t1 = datetime.time(1, 2, 3)
-t2 = datetime.time(2, 3, 4)
+s1 = '08:59:59'
+s2 = '10:15:49' # for example
 
-print(t2,t1)
+print(timeVerify(s1))
+print(timeVerify(s2))
 
-
-#s1 = '10:33:26'
-#s2 = '11:15:49' # for example
-#FMT = '%H:%M:%S'
-#tdelta = datetime.strptime(s2, FMT) - datetime.strptime(s1, FMT)
+FMT = '%H:%M:%S'
+tdelta = datetime.datetime.strptime(s2, FMT) - datetime.datetime.strptime(s1, FMT)
+print (tdelta)
